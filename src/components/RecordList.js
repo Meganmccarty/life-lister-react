406df87bcf4d39e217f83cd42aa75605715259a3
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-08 12:13:47 -0400

    Add filter and sort functionality

--- src/components/RecordList.js
+++ src/components/RecordList.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+
 import RecordCard from './RecordCard';
 
 function RecordList({ records, onPatchRecord, onDeleteRecord }) {
+    const [filter, setFilter] = useState("--");
+    const [sort, setSort] = useState("--");
 
-    const displayRecords = records.map(record => {
+    const displayRecords = records
+        .filter(record => {
+            return filter !== "--" ? record.category === filter : record;
+        })
+        .sort((a, b) => {
+            if (sort === "Most Recent") {
+                return b.date_seen.localeCompare(a.date_seen);
+            } else if (sort === "Oldest") {
+                return a.date_seen.localeCompare(b.date_seen);
+            } else {
+                return records;
+            }
+        })
+        .map(record => {
         return (
             <RecordCard
                 key={record.id}
@@ -16,11 +33,33 @@ function RecordList({ records, onPatchRecord, onDeleteRecord }) {
             />
         );
     });
-    
+
     return (
-        <div className="record-list">
-            {displayRecords}
-        </div>
+        <>
+            <div>
+                <label htmlFor="filter-category">Filter by Category: </label>
+                <select id="filter-category" value={filter} onChange={(e) => setFilter(e.target.value)}>
+                    <option>--</option>
+                    <option>Bird</option>
+                    <option>Butterfly</option>
+                    <option>Fish</option>
+                    <option>Insect</option>
+                    <option>Mammal</option>
+                    <option>Plant</option>
+                    <option>Reptile</option>
+                    <option>Other</option>
+                </select>
+                <label htmlFor="sort-date">Sort by Date Seen: </label>
+                <select id="sort-date" value={sort} onChange={(e) => setSort(e.target.value)}>
+                    <option>--</option>
+                    <option>Most Recent</option>
+                    <option>Oldest</option>
+                </select>
+            </div>
+            <div className="record-list">
+                {displayRecords}
+            </div>
+        </>
     );
 };
 
